815be94b883895dc041791fdf91233e04112ba4a
mytopdog <essmale2005@gmail.com>
2020-09-08 14:11:54 +0100

    Fixed wrong rule

--- src/service/rule/Storage.js
+++ src/service/rule/Storage.js
@@ -128,10 +128,10 @@ export default new CustomCommandRuleGroup({
         }),
         new CustomCommandRule({
             id: "d5bf0083-26c2-4fbc-9b1e-46b75bc6bc5d",
-            name: "Delete item % in % to %",
+            name: "Delete item % in %",
             description: "Set an item in a storage collection.",
             params: ["string", "string", "string"],
-            callback: async function SetCollectionItem(item, name, val) {
+            callback: async function SetCollectionItem(item, name,) {
                 const service = this.client.StorageService;
                 const storage = await service.getStorage(this.guild);
 
@@ -146,7 +146,7 @@ export default new CustomCommandRuleGroup({
                         const collection = storage.createCollection(name);
 
                         if (collection) {
-                            collection.deleteItem(item, val);
+                            collection.deleteItem(item);
                         }
 
                         await storage.save();
