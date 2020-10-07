f59870e777acb36f5143661a65b2265828cd952c
mytopdog <essmale2005@gmail.com>
2020-10-07 21:42:03 +0100

    AAAAfixed again

--- src/client/command/music/mod.js
+++ src/client/command/music/mod.js
@@ -32,7 +32,7 @@ export default new BotModule({
                     return await this.reply("error", "You must be in a voice channel to use this command.");
                 }
                 
-                if (!player.channel?.id || message.member.voice.channelID !== player.channel?.id) {
+                if (player.channel?.id && message.member.voice.channelID !== player.channel?.id) {
                     return await this.reply("error", "You must be in the voice channel to use this command.");
                 }
 
@@ -68,7 +68,7 @@ export default new BotModule({
                     return await this.reply("error", "You must be in a voice channel to use this command.");
                 }
                 
-                if (!player.channel?.id || message.member.voice.channelID !== player.channel?.id) {
+                if (player.channel?.id && message.member.voice.channelID !== player.channel?.id) {
                     return await this.reply("error", "You must be in the voice channel to use this command.");
                 }
 
@@ -110,7 +110,7 @@ export default new BotModule({
                     return await this.reply("error", "You must be in a voice channel to use this command.");
                 }
 
-                if (!player.channel?.id || message.member.voice.channelID !== player.channel?.id) {
+                if (player.channel?.id && message.member.voice.channelID !== player.channel?.id) {
                     return await this.reply("error", "You must be in the voice channel to use this command.");
                 }
 
@@ -153,7 +153,7 @@ export default new BotModule({
                     return await this.reply("error", "You must be in a voice channel to use this command.");
                 }
                 
-                if (!player.channel?.id || message.member.voice.channelID !== player.channel?.id) {
+                if (player.channel?.id && message.member.voice.channelID !== player.channel?.id) {
                     return await this.reply("error", "You must be in the voice channel to use this command.");
                 }
 
@@ -226,7 +226,7 @@ export default new BotModule({
                     return await this.reply("error", "You must be in a voice channel to use this command.");
                 }
                 
-                if (!player.channel?.id || message.member.voice.channelID !== player.channel?.id) {
+                if (player.channel?.id && message.member.voice.channelID !== player.channel?.id) {
                     return await this.reply("error", "You must be in the voice channel to use this command.");
                 }
 
